6ae5c20c202c7a3b015cd8d9c21d09a0e2fdf3ee
TrogRog <xyah86@yandex.ru>
2022-08-01 20:55:34 +0400

    maven

---
+++ src/main/java/home/Hello.java
@@ -0,0 +1,12 @@
+package home;
+
+
+public class Hello {
+    /**
+     * jknbgkljdsnhlk
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
