31623124dfaf0005e888ed06be0a3e6df6af0e3f
TrogRog <xyah86@yandex.ru>
2022-08-02 21:44:05 +0400

    maven

--- src/main/java/home/Hello.java
+++ src/main/java/home/Hello.java
@@ -1,12 +1,23 @@
 package home;
 
-
+/**
+ * @author Gusarov Stanislav
+ * @version 1
+ */
 public class Hello {
     /**
-     * jknbgkljdsnhlk
+     * Program write "Hello" on display
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String x = "Hello";
+        println(x);
+    }
+
+    /**
+     *   @param str the string to display.
+     */
+    public static void println(String str){
+        System.out.println(str);
     }
 }
